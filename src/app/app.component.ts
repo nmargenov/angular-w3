30f3739889cf180537b3d2b21962b7a9b9fe977a
nmargenov <nikolay_margenov@yahoo.com>
2024-04-26 22:55:59 +0300

    sceleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsComponent } from './forms/forms.component';
+import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsComponent,ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
