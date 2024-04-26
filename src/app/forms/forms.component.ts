2d66acf2daa272b75c7efb9ce76240b7e77c4987
nmargenov <nikolay_margenov@yahoo.com>
2024-04-26 23:28:53 +0300

    form html css and all the needed logic

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.css'
 })
 export class FormsComponent {
+  title:string = '';
+  description:string = '';
 
+
+  onSubmit() {
+    console.log(this.title);
+  }
 }
